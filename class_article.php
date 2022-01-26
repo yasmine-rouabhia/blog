9502d1c389617a1d9886e742c1fdf538e84cabfc
chadhilati-mansoibou <chadhilati.mansoibou@laplateforme.io>
2022-01-26 10:46:25 +0100

    ajout class article

---
+++ class_article.php
@@ -0,0 +1,16 @@
+<?php
+
+class Article
+{
+    private $id;
+    public $articles;
+    public $id_utilisateur; 
+    public$id_categorie;
+    public $date;
+
+    public function __construct()
+    {
+
+    }
+
+}
